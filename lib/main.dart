64358b293651047258c3b0b99e51aad026b4228f
Nima <nimamoradi41@gmail.com>
2021-07-03 12:22:32 +0430

    Two

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uiproject/Constant.dart';
 import 'package:uiproject/appbar.dart';
@@ -45,7 +46,64 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<dataCategory> datas=[];
+  List<String> experts=[];
 
+
+
+  Widget  GetWEige()
+  {
+
+    return  SafeArea(
+      child: Scaffold(
+          body:Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: BackColor,
+            child: Column(
+              children: [
+                appbar(),
+                ListView.builder(
+                    physics:ClampingScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: datas.length,
+                    itemBuilder: (ctx,index){
+                      return Container(
+                        margin: EdgeInsets.all(4),
+                        child: Card(
+                          elevation: ElevatoinApp,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                    margin: EdgeInsets.only(left: 8),
+                                    child: Icon(Icons.arrow_back_ios,color: Colors.black26,)),
+                                Expanded(child: Container(
+                                  margin: EdgeInsets.only(right: 8),
+                                  child:
+                                  Text(datas[index].title.toString(),
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily:  'iranyekanbold',
+                                      decoration: TextDecoration.none,),),)),
+                                SvgPicture.asset('assets/blacksmith.svg',width: 60,height: 60,)
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    })
+              ],
+            ),
+          )
+
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -56,6 +114,13 @@ class _MyHomePageState extends State<MyHomePage> {
     datas.add(dataCategory('کاشی کاری'));
     datas.add(dataCategory('قیر گونی'));
     datas.add(dataCategory('بنایی جزیی'));
+
+    experts.add('تیم متخصص پلاس');
+    experts.add('محمد صالحی');
+    experts.add('اصغر کامرانی');
+    experts.add('مرتضی فرهادی');
+    experts.add('علی رشیدی');
+
   }
 
 
@@ -73,45 +138,174 @@ class _MyHomePageState extends State<MyHomePage> {
           width: double.infinity,
           height: double.infinity,
           color: BackColor,
-          child: Column(
-            children: [
-              appbar(),
-              ListView.builder(
-                  physics:ClampingScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: datas.length,
-                  itemBuilder: (ctx,index){
-                    return Container(
-                      margin: EdgeInsets.all(4),
-                      child: Card(
-                        elevation: ElevatoinApp,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Container(
-                                   margin: EdgeInsets.only(left: 8),
-                                  child: Icon(Icons.arrow_back_ios,color: Colors.black26,)),
-                              Expanded(child: Container(
-                                margin: EdgeInsets.only(right: 8),
-                                child:
-                              Text(datas[index].title.toString(),
-                                textAlign: TextAlign.end,
-                                style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontFamily:  'iranyekanbold',
-                                decoration: TextDecoration.none,),),)),
-                              SvgPicture.asset('assets/blacksmith.svg',width: 60,height: 60,)
-                            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                appbar(),
+                ListView.builder(
+                    physics:ClampingScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: datas.length,
+                    itemBuilder: (ctx,index){
+                      return Container(
+                        margin: EdgeInsets.all(4),
+                        child: Card(
+                          elevation: ElevatoinApp,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                              margin: EdgeInsets.only(left: 8),
+                                              decoration: BoxDecoration(
+                                                  color: Colors.green,
+                                                  borderRadius: BorderRadius.circular(8)
+                                              ),
+                                              child: Center(child: Padding(
+                                                padding: const EdgeInsets.all(8.0),
+                                                child: Text('انتخاب متخصص',
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                    fontSize: 10,
+                                                    color: Colors.black,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily:  'iranyekanbold',
+                                                    decoration: TextDecoration.none,),),
+                                              ))),
+                                          Expanded(child: Container(
+                                            margin: EdgeInsets.only(right: 8),
+                                            child:
+                                            Column(
+                                              children: [
+                                                Container(
+                                                  width: double.infinity,
+                                                  child: Text(experts[index].toString(),
+                                                    textAlign: TextAlign.end,
+                                                    style: TextStyle(
+                                                      fontSize: 14,
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontFamily:  'iranyekanbold',
+                                                      decoration: TextDecoration.none,),),
+                                                ),
+                                                Row(children: [
+                                                  Expanded(
+                                                    child: Center(
+                                                      child: Align(
+                                                        alignment: Alignment.bottomRight,
+                                                        child: RatingBar.builder(
+                                                          initialRating: 5,
+                                                          minRating: 1,
+                                                          itemSize: 15,
+                                                          itemCount: 5,
+                                                          itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                                                          itemBuilder: (context, _) => Icon(
+                                                            Icons.star,
+                                                            color: Colors.amber,
+                                                          ),
+                                                          onRatingUpdate: (rating) {
+                                                            print(rating);
+                                                          },
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Text(':امتیاز',
+                                                    textAlign: TextAlign.end,
+                                                    style: TextStyle(
+                                                      fontSize: 10,
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontFamily:  'iranyekanbold',
+                                                      decoration: TextDecoration.none,),),
+                                                ],),
+                                                Container(
+                                                  margin: EdgeInsets.only(top: 8),
+                                                  width: double.infinity,
+                                                  child: Text('32 نظر ثبت شده',
+                                                    textAlign: TextAlign.end,
+                                                    style: TextStyle(
+                                                      fontSize: 8,
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold,
+                                                      fontFamily:  'iranyekanbold',
+                                                      decoration: TextDecoration.none,),),
+                                                ),
+
+
+
+
+                                              ],
+                                            ),)),
+                                        ],
+                                      ),
+
+                                      experts[index]=='تیم متخصص پلاس' ? Container(
+                                        margin: EdgeInsets.all(4),
+                                        width: double.infinity,
+                                        child: Expanded(
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            mainAxisAlignment: MainAxisAlignment.end,
+                                            children: [
+                                              Container(
+                                                margin: EdgeInsets.only(right: 2),
+                                                child: Text('ثضمین بهترین قیمت',
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                    fontSize: 8,
+                                                    color: Colors.black54,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily:  'iranyekanbold',
+                                                    decoration: TextDecoration.none,),),
+                                              ),
+                                              Icon(Icons.monetization_on,color: Colors.yellow,size: 15,),
+                                              Container(
+                                                margin: EdgeInsets.only(right: 2,left: 2),
+                                                child: Text('ضمانت انجام کار',
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                    fontSize: 8,
+                                                    color: Colors.black54,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily:  'iranyekanbold',
+                                                    decoration: TextDecoration.none,),),
+                                              ),
+                                              Icon(Icons.stars_sharp,color: Colors.yellow,size: 15,),
+                                              Container(
+                                                margin: EdgeInsets.only(right: 2,left: 2),
+                                                child: Text('نظارت مهندسی',
+                                                  textAlign: TextAlign.center,
+                                                  style: TextStyle(
+                                                    fontSize: 8,
+                                                    color: Colors.black54,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily:  'iranyekanbold',
+                                                    decoration: TextDecoration.none,),),
+                                              ),
+                                              Icon(Icons.supervised_user_circle_outlined,color: Colors.black26,size: 15,)
+                                            ],
+                                          ),
+                                        ),
+                                      ):Container()
+                                    ],
+                                  ),
+                                ),
+                                SvgPicture.asset('assets/male.svg',width: 60,height: 60,)
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  })
-            ],
+                      );
+                    })
+              ],
+            ),
           ),
         )
 
